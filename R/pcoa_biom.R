9df9dceb6ed3e4b06f024f335c74925863f4ed0b
Jonathan Badger <jhbadger@gmail.com>
2017-09-28 13:18:22 -0400

    pcoa_subset_biom

--- R/pcoa_biom.R
+++ R/pcoa_biom.R
@@ -29,3 +29,47 @@ pcoa_biom <- function(biom, method = "PCoA", distance = "unifrac", color_categor
   }
   p +  theme_light()
 }
+
+#' Make a scatter PCA/PCOA plot of phyloseq object with complete biom and subset of samples -- useful for conserved axes
+#' 
+#' @param biom phyloseq object
+#' @param method data reduction method to use (default PCoA; see phyloseq ordinate)
+#' @param distance distance measure to use (default unifrac)
+#' @param color_category variable in sample data (if any) to color points by
+#' @param shape_category variable in sample data (if any) to distinguish points by symbol
+#' @param weighted use weighted distance (normally only useful for unifrac)
+#' @param title title of plot
+#' @param color vector of colors to use if you don't like the defaults
+#' @param shapes vector of shapes to use if you don't like the defaults
+#' @export
+#' @examples
+#' pcoa_subset_biom
+
+pcoa_subset_biom <- function(biom, method = "PCoA", distance = "unifrac", samples, 
+                             color_category = NULL, shape_category=NULL, weighted = FALSE, 
+                             title = NULL, colors = NULL, shapes = NULL, seed = 100) {
+  set.seed(seed)
+  ordu <- ordinate(biom, method = method, distance = distance, weighted = weighted)
+  plt <- plot_ordination(biom, ordu, color = color_category, 
+                         shape = shape_category, justDF = TRUE)
+  plt <- plt[samples, ]
+  plt <- plt[complete.cases(plt),]
+  p <- ggplot(plt, aes_string(x="Axis.1", y="Axis.2", color=color_category,
+                              shape = shape_category))+geom_point()
+  if (!is.null(colors)) {
+    p <- p + scale_color_manual(values=colors)
+  }
+  if(!is.null(shapes)) {
+    p <- p + scale_shape_manual(values=shapes)
+  }
+  if (!is.null(title)) {
+    p <- p + ggtitle(title)
+  }
+  p <- p + theme_light()
+  p <- p + 
+    xlab(paste0(paste0("Axis.1 [",
+                       round(ordu$values$Relative_eig[1]*100,1)), "%]"))
+  p + 
+    ylab(paste0(paste0("Axis.2 [",
+                       round(ordu$values$Relative_eig[2]*100,1)), "%]"))
+}
