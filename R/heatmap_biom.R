5f69697acc8518d61ec6ad32176a692a088fe221
Jonathan Badger <jhbadger@gmail.com>
2017-12-21 11:38:14 -0500

    plotMRheatmap based heatmap

--- R/heatmap_biom.R
+++ R/heatmap_biom.R
@@ -2,16 +2,30 @@
 #'
 #' @param biom phyloseq object
 #' @param rank string giving taxonomic rank to compare at
-#' @param group  vector identifying the group for each slice
+#' @param group vector identifying the group
+#' @param title string giving the title
+#' @param ntop maximum number of rows
 #' @export
 #' @examples
 #' heatmap_biom()
 
-heatmap_biom <- function(biom, rank, group, title = "") {
-  biom <- tax_glom(biom, rank)
-  plot_heatmap(biom, taxa.label = rank, sample.label = group) +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, 
-                                     size = min(8, 350/nsamples(biom))), 
-          axis.text.y = element_text(size = min(10, 350/ntaxa(biom))))  +
-    ggtitle(title) 
+heatmap_biom <- function(biom, rank="LKT", group,
+                          title="", ntop=40) {
+  phyloseq <- tax_glom(biom, rank)
+  taxa_names(biom) <- make.names(tax_table(biom)[,rank],
+                                     unique = TRUE)
+  ntop <- min(ntaxa(biom)/2, ntop)
+  obj <- phyloseq_to_metagenomeSeq(biom)
+  trials <- pData(obj)[ , which(colnames(pData(obj))==group)]
+  heatmapColColors <- brewer.pal(12,"Set3")[as.integer(factor(trials))]
+  heatmapCols <- colorRampPalette((brewer.pal(9, "YlOrRd")))(50)
+  par(oma=c(5,4,3,12)+0.1, xpd=TRUE)
+  plotMRheatmap(obj, n = ntop, norm = TRUE, log = TRUE,
+                fun = sd, cexRow = 0.5, cexCol = 0.5, trace = "none",
+                col = heatmapCols, ColSideColors = heatmapColColors,
+                key=FALSE, main=title, cex.main=0.35)
+  cores <- unique(heatmapColColors)
+  labels <- unique(trials)
+  legend("topright", inset=c(1,0), legend=labels, fill=cores,
+         border=TRUE, bty="n", y.intersp = 0.7, cex=0.5)
 }
